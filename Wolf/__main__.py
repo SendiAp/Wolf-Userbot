88431d05ac62a9f6686c82457e2cdf4ba4f0bd77
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-07 23:13:08 +0700

    Create __main__.py

---
+++ Wolf/__main__.py
@@ -0,0 +1,44 @@
+""" Userbot start point """
+
+
+import sys
+from importlib import import_module
+from platform import python_version
+
+from pytgcalls import __version__ as pytgcalls
+from pytgcalls import idle
+from telethon import version
+
+from userbot import BOT_TOKEN
+from userbot import BOT_VER as ubotversion
+from userbot import LOGS, bot
+from userbot.clients import wolf_userbot_on, wolfmulti
+from userbot.modules import ALL_MODULES
+from userbot.utils import autobot, checking
+
+try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
+    client = wolfmulti()
+    total = 5 - client
+    LOGS.info(f"Total Clients = {total} User")
+    LOGS.info(f"Python Version - {python_version()}")
+    LOGS.info(f"Telethon Version - {version.__version__}")
+    LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
+    LOGS.info(f"Wolf-Userbot Version - {ubotversion} [🐺 BERHASIL DIAKTIFKAN! 🐺]")
+except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
+    pass
+except BaseException as e:
+    LOGS.info(str(e), exc_info=True)
+    sys.exit(1)
+
+
+bot.loop.run_until_complete(checking())
+bot.loop.run_until_complete(wolf_userbot_on())
+if not BOT_TOKEN:
+    bot.loop.run_until_complete(autobot())
+idle()
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
