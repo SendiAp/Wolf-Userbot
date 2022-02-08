e69aeccec2947aa627072a53ad84f0c7356e4305
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-08 11:50:17 +0700

    Create session.py

---
+++ Wolf/core/session.py
@@ -0,0 +1,43 @@
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+
+from ..Config import Config
+from .client import DrgUserBotClient
+
+__version__ = "beta"
+
+loop = None
+
+if Config.STRING_SESSION:
+    session = StringSession(str(Config.STRING_SESSION))
+else:
+    session = "Wolf-Userbot"
+
+try:
+    wolf = WolfUserBotClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"STRING_SESSION - {str(e)}")
+    sys.exit()
+
+
+wolf.tgbot = tgbot = WolfUserBotClient(
+    session="DrgTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.TG_BOT_TOKEN)
